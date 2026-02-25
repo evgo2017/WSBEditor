56f52bd8545ed73c41bb0af8ecf5889a0f5e4b83
evgo2017 <evgo2017@qq.com>
2026-02-26 00:08:32 +0800

    feat: baidutongji

--- src/main.js
+++ src/main.js
@@ -4,5 +4,18 @@ import App from './App.vue'
 import i18n from './i18n'
 
 const app = createApp(App)
+
+// Baidu Analytics
+const baiduId = import.meta.env.VITE_BAIDU_TONGJI_ID
+if (baiduId) {
+    window._hmt = window._hmt || [];
+    (function () {
+        const hm = document.createElement("script")
+        hm.src = `https://hm.baidu.com/hm.js?${baiduId}`
+        const s = document.getElementsByTagName("script")[0]
+        s.parentNode.insertBefore(hm, s)
+    })()
+}
+
 app.use(i18n)
 app.mount('#app')
