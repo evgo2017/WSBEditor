7a28c9a62e11c9ee43d12c1e1011407921de0998
evgo2017 <evgo2017@qq.com>
2026-02-27 21:20:41 +0800

    fix(tongji): url 路径 and dev模式

--- src/main.js
+++ src/main.js
@@ -7,8 +7,19 @@ const app = createApp(App)
 
 // Baidu Analytics
 const baiduId = import.meta.env.VITE_BAIDU_TONGJI_ID
-if (baiduId) {
+if (baiduId && !import.meta.env.DEV) {
+    const baseUrl = import.meta.env.BASE_URL || '/'
+    const normalizedBase = baseUrl === '/' ? '' : baseUrl.replace(/\/$/, '')
+    const pathname = window.location.pathname || '/'
+    const trackedPath = pathname.startsWith(normalizedBase)
+        ? pathname
+        : `${normalizedBase}${pathname.startsWith('/') ? pathname : `/${pathname}`}`
+    const trackedUrl = `${trackedPath}${window.location.search}${window.location.hash}`
+
     window._hmt = window._hmt || [];
+    window._hmt.push(['_setAutoPageview', false])
+    window._hmt.push(['_trackPageview', trackedUrl])
+
     (function () {
         const hm = document.createElement("script")
         hm.src = `https://hm.baidu.com/hm.js?${baiduId}`
